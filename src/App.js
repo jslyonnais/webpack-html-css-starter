fa81f7eb3d6e4217e93f74dc9fc5dc2dd37258a7
Jean-Simon Lyonnais <iam@jslyonnais.com>
2018-08-29 08:27:58 -0400

    Making a sass version :-)

--- src/App.js
+++ src/App.js
@@ -1,1 +1,5 @@
-import styles from './assets/style/main.css';
+import styles from './assets/style/style.css';
+
+// Import all SVG files from folder
+function requireAll(r) { r.keys().forEach(r); }
+requireAll(require.context('./assets/svg/', true, /\.svg$/));
